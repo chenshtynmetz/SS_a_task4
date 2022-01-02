9d85eb759c9693cbbdeebaaa0f96b59d186aa4ae
chenshtynmetz <jiayhb@gmail.com>
2022-01-02 21:21:26 +0200

    cc

--- graph.h
+++ graph.h
@@ -1,12 +1,27 @@
-//
-// Created by tavor on 02/01/2022.
-//
+#ifndef GRAPH_
+#define GRAPH_
 
-//#ifndef RENANA_REN_H
-//#define RENANA_REN_H
-//
-//
-//
-//int add(int a, int b);
-//
-//#endif //RENANA_REN_H
+typedef struct GRAPH_NODE_ *pnode;;
+
+typedef struct edge_ {
+    int weight;
+    pnode endpoint;
+    struct edge_ *next;
+} edge, *pedge;
+
+
+typedef struct GRAPH_NODE_ {
+    int node_num;
+    pedge edges;
+    struct GRAPH_NODE_ *next;
+} node, *pnode;
+
+void build_graph_cmd(pnode *head);
+void insert_node_cmd(pnode *head);
+void delete_node_cmd(pnode *head);
+void printGraph_cmd(pnode head); //for self debug
+void deleteGraph_cmd(pnode* head);
+void shortsPath_cmd(pnode head);
+void TSP_cmd(pnode head);
+
+#endif
